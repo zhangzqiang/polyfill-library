92a03af7ad84f3bd575ca6177c35762460e419bc
Jake Champion <me@jakechampion.name>
2020-09-30 17:17:26 +0100

    Update Number.IsSafeInteger to use the new definition steps

--- polyfills/Number/isSafeInteger/polyfill.js
+++ polyfills/Number/isSafeInteger/polyfill.js
@@ -1,24 +1,13 @@
 /* global CreateMethodProperty, Type, ToInteger */
 // 20.1.2.5. Number.isSafeInteger ( number )
 CreateMethodProperty(Number, 'isSafeInteger', function isSafeInteger(number) {
-	// 1. If Type(number) is not Number, return false.
-	if (Type(number) !== 'number') {
-		return false;
+	// 1. If ! IsInteger(number) is true, then
+	if (IsInteger(number)) {
+		// 1.a. If abs(number) ≤ 253 - 1, return true.
+		if (Math.abs(number) <= (Math.pow(2, 53) - 1)) {
+			return true;
+		}
 	}
-	// 2. If number is NaN, +∞, or -∞, return false.
-	if (isNaN(number) || number === Infinity || number === -Infinity) {
-		return false;
-	}
-	// 3. Let integer be ToInteger(number).
-	var integer = ToInteger(number);
-	// 4. If integer is not equal to number, return false.
-	if (integer !== number) {
-		return false;
-	}
-	// 5. If abs(integer) ≤ 2^53-1, return true.
-	if (Math.abs(integer) <= (Math.pow(2, 53) - 1)) {
-		return true;
-	}
-	// 6. Otherwise, return false.
+	// 2. Return false.
 	return false;
 });
