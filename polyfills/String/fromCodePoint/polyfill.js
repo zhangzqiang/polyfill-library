50e8d3535915639d6766f4b145c210dfa04528ac
jake <me@jakechampion.name>
2020-10-27 23:22:17 +0000

    fix string.fromcodepoint to be memory efficient

--- polyfills/String/fromCodePoint/polyfill.js
+++ polyfills/String/fromCodePoint/polyfill.js
@@ -2,12 +2,14 @@
 
 // 21.1.2.2. String.fromCodePoint ( ...codePoints )
 CreateMethodProperty(String, 'fromCodePoint', function fromCodePoint(_ /*...codePoints*/) {
+	// Polyfill.io - List to store the characters whilst iterating over the code points.
+	var result = [];
 	var codePoints = arguments;
 	// 1. Let elements be a new empty List.
 	var elements = [];
 	// 2. For each element next of codePoints, do
 	for (var i = 0; i < codePoints.length; i++) {
-		var next = codePoints[1];
+		var next = codePoints[i];
 		// 2.a. Let nextCP be ? ToNumber(next).
 		var nextCP = ToNumber(next);
 		// 2.b. If ! IsInteger(nextCP) is false, throw a RangeError exception.
@@ -19,10 +21,10 @@ CreateMethodProperty(String, 'fromCodePoint', function fromCodePoint(_ /*...code
 			throw new RangeError('Invalid code point ' + Object.prototype.toString.call(nextCP));
 		}
 		// 2.d. Append the elements of ! CodePointToUTF16CodeUnits(nextCP) to the end of elements.
-		elements.concat(CodePointToUTF16CodeUnits(nextCP));
+		// elements = elements.concat(CodePointToUTF16CodeUnits(nextCP));
+		// Polyfill.io - Retrieving the characters whilst iterating enables the function to work in a memory efficient and performant way.
+		result.push(String.fromCharCode.apply(null, CodePointToUTF16CodeUnits(nextCP)));
 	}
 	// 3. Return the String value whose code units are, in order, the elements in the List elements. If codePoints is empty, the empty String is returned.
-	return elements.reduce(function(previous, current) {
-		return previous + String.fromCharCode(current);
-	}, '');
+	return codePoints.length === 0 ? '' : result.join('');
 });
--- polyfills/String/fromCodePoint/tests.js
+++ polyfills/String/fromCodePoint/tests.js
@@ -17,58 +17,117 @@ it('is not enumerable', function () {
 	proclaim.isNotEnumerable(String, 'fromCodePoint');
 });
 
-
-it('works as expected', function () {
-	this.timeout(10000);
-
-	proclaim.strictEqual(String.fromCodePoint(''), '\0');
+it('returns empty string if called with no arguments', function () {
 	proclaim.strictEqual(String.fromCodePoint(), '');
-	proclaim.strictEqual(String.fromCodePoint(-0), '\0');
-	proclaim.strictEqual(String.fromCodePoint(0), '\0');
-	proclaim.strictEqual(String.fromCodePoint(0x1D306), '\uD834\uDF06');
-	proclaim.strictEqual(String.fromCodePoint(0x1D306, 0x61, 0x1D307), '\uD834\uDF06a\uD834\uDF07');
-	proclaim.strictEqual(String.fromCodePoint(0x61, 0x62, 0x1D307), 'ab\uD834\uDF07');
-	proclaim.strictEqual(String.fromCodePoint(false), '\0');
-	proclaim.strictEqual(String.fromCodePoint(null), '\0');
+});
+
+it('throws when called with a string which is not a number', function () {
 	proclaim["throws"](function () {
 		String.fromCodePoint('_');
 	}, RangeError);
+});
+
+it('throws when called with a string which is positive infinity', function () {
 	proclaim["throws"](function () {
 		String.fromCodePoint('+Infinity');
 	}, RangeError);
+});
+
+it('throws when called with a string which is negative infinity', function () {
 	proclaim["throws"](function () {
 		String.fromCodePoint('-Infinity');
 	}, RangeError);
+});
+
+it('throws when called with a negative number', function () {
 	proclaim["throws"](function () {
 		String.fromCodePoint(-1);
 	}, RangeError);
+});
+
+it('throws when called with a number which is beyond the codepoint space', function () {
 	proclaim["throws"](function () {
 		String.fromCodePoint(0x10FFFF + 1);
 	}, RangeError);
+});
+
+it('throws when called with a non integer number', function () {
 	proclaim["throws"](function () {
 		String.fromCodePoint(3.14);
 	}, RangeError);
+});
+
+it('throws when called with a non integer number', function () {
 	proclaim["throws"](function () {
 		String.fromCodePoint(3e-2);
 	}, RangeError);
+});
+
+it('throws when called with negative infinity', function () {
 	proclaim["throws"](function () {
 		String.fromCodePoint(-Infinity);
 	}, RangeError);
+});
+
+it('throws when called with positive infinity', function () {
 	proclaim["throws"](function () {
 		String.fromCodePoint(Infinity);
 	}, RangeError);
+});
+
+it('throws when called with NaN', function () {
 	proclaim["throws"](function () {
 		String.fromCodePoint(NaN);
 	}, RangeError);
+});
+
+it('throws when called with undefined', function () {
 	proclaim["throws"](function () {
 		String.fromCodePoint(undefined);
 	}, RangeError);
+});
+
+it('throws when called with an object', function () {
 	proclaim["throws"](function () {
 		String.fromCodePoint({});
 	}, RangeError);
+});
+
+it('throws when called with a regular expression', function () {
 	proclaim["throws"](function () {
 		String.fromCodePoint(/./);
 	}, RangeError);
+});
+
+it('returns "\0" if called with an empty string', function () {
+	proclaim.strictEqual(String.fromCodePoint(''), '\0');
+});
+
+it('returns "\0" if called with negative 0', function () {
+	proclaim.strictEqual(String.fromCodePoint(-0), '\0');
+});
+
+it('returns "\0" if called with positive 0', function () {
+	proclaim.strictEqual(String.fromCodePoint(0), '\0');
+});
+
+it('returns "\0" if called with false', function () {
+	proclaim.strictEqual(String.fromCodePoint(false), '\0');
+});
+
+it('returns "\0" if called with null', function () {
+	proclaim.strictEqual(String.fromCodePoint(null), '\0');
+});
+
+it('handles astral codepoints', function(){
+	proclaim.strictEqual(String.fromCodePoint(0x1D306), '\uD834\uDF06');
+	proclaim.strictEqual(String.fromCodePoint(0x1D306, 0x61, 0x1D307), '\uD834\uDF06a\uD834\uDF07');
+	proclaim.strictEqual(String.fromCodePoint(0x61, 0x62, 0x1D307), 'ab\uD834\uDF07');
+});
+
+it('calls valueOf on the passed in argument', function () {
+	this.timeout(10000);
+
 	var tmp = 0x60;
 	proclaim.strictEqual(String.fromCodePoint({
 		valueOf: function () {
@@ -76,18 +135,21 @@ it('works as expected', function () {
 		}
 	}), 'a');
 	proclaim.strictEqual(tmp, 0x61);
-	proclaim.doesNotThrow(function() {
+});
+
+it('does not throw on long argument lists', function () {
+	proclaim.doesNotThrow(function () {
 		var result = [];
 		// one code unit per item in array
-		for (var i =0; i < 49152; i++) {
+		for (var i = 0; i < 49152; i++) {
 			result.push(0);
 		}
 		String.fromCodePoint.apply(null, result);
 	});
-	proclaim.doesNotThrow(function() {
+	proclaim.doesNotThrow(function () {
 		var result = [];
 		// two code units per item in array
-		for (var i =0; i < 49152; i++) {
+		for (var i = 0; i < 49152; i++) {
 			result.push(0xFFFF + 1);
 		}
 		String.fromCodePoint.apply(null, result);
